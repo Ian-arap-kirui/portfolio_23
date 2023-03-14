685a592357121cc64fdb0c8c5e45ed2b31b275e7
Ian-Mark <ianarapkirui@gmail.com>
2023-03-14 15:15:04 +0300

    hero 3d sphere: added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { MeshDistortMaterial, OrbitControls, Sphere } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
 import Navbar from "./Navbar";
 
@@ -58,8 +60,8 @@ const Right = styled.div`
   position: relative;
 `;
 const Img = styled.img`
-  width: 800px;
-  height: 800px;
+  width: 700px;
+  height: 700px;
   object-fit: contain;
   position: absolute;
   top: 0;
@@ -91,6 +93,21 @@ const Hero = () => {
           <Button>Learn More</Button>
         </Left>
         <Right>
+          <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
+                  color="#4b236a"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
           <Img src="./img/moon.png" />
         </Right>
       </Container>
