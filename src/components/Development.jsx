825f9f47f3be5be66e5dbbf6e17ad703820cd9e4
Ian-Mark <ianarapkirui@gmail.com>
2023-03-20 12:07:39 +0300

    mac 3d update

--- src/components/Development.jsx
+++ src/components/Development.jsx
@@ -6,15 +6,13 @@ import styled from "styled-components";
 
 const Desc = styled.div`
   width: 200px;
-  height: 100px;
-  color: black;
+  height: 70px;
   padding: 20px;
   background-color: white;
   border-radius: 10px;
   position: absolute;
   top: 200px;
   right: 100px;
-
   @media only screen and (max-width: 768px) {
     top: 0;
     bottom: 0;
